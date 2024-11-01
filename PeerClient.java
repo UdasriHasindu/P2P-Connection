f096e93cb4de9758208085b84e5c3a6d9bb8527f
u-sri2002 <usrimart@gmail.com>
2024-11-01 07:53:24 +0530

    build a connection p2p

--- PeerClient.java
+++ PeerClient.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class PeerClient {
     public static void main(String[] args) {
-        System.out.println("Connecting to server...");
+
+        try {
+            System.out.println("Connecting to server...");
+            Socket socket = new Socket("localhost", 5454);
+            System.out.println("Connected to " + socket.getRemoteSocketAddress());
+
+
+            socket.close(); // Close socket when done
+        } catch (IOException e) {
+            System.err.println("Client exception: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
-
--- PeerServer.java
+++ PeerServer.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class PeerServer {
     public static void main(String[] args) {
-        System.out.println("PeerServer starting...");
+
+        try (ServerSocket serverSocket = new ServerSocket(5454)) {
+            System.out.println("Server starting...");
+            Socket socket = serverSocket.accept();
+            System.out.println("New connection from " + socket.getRemoteSocketAddress());
+
+            socket.close(); // Close socket when done
+        } catch (IOException e) {
+            System.err.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
