4e87f4701d9bd39f453599a56350893c1ac882d1
u-sri2002 <usrimart@gmail.com>
2024-11-01 07:18:52 +0530

    Initial commit

---
+++ PeerClient.java
@@ -0,0 +1,6 @@
+public class PeerClient {
+    public static void main(String[] args) {
+        System.out.println("Connecting to server...");
+    }
+}
+
---
+++ PeerServer.java
@@ -0,0 +1,5 @@
+public class PeerServer {
+    public static void main(String[] args) {
+        System.out.println("PeerServer starting...");
+    }
+}
