f4810d731e23cd4fd0def004f4105df731178c94
u-sri2002 <usrimart@gmail.com>
2024-11-01 08:52:37 +0530

    establish connection to send message

--- PeerClient.java
+++ PeerClient.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class PeerClient {
@@ -9,6 +12,19 @@ public class PeerClient {
             Socket socket = new Socket("localhost", 5454);
             System.out.println("Connected to " + socket.getRemoteSocketAddress());
 
+            // Get user input
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the message: ");
+            String message = userInput.readLine();
+
+            // Send message to server
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println(message);
+
+            // Receive and print server's response
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String serverResponse = in.readLine();
+            System.out.println("Server response: " + serverResponse);
 
             socket.close(); // Close socket when done
         } catch (IOException e) {
--- PeerServer.java
+++ PeerServer.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,6 +12,17 @@ public class PeerServer {
             System.out.println("Server starting...");
             Socket socket = serverSocket.accept();
             System.out.println("New connection from " + socket.getRemoteSocketAddress());
+
+            // Create input and output streams
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Read message from client
+            String msg = in.readLine();
+            System.out.println("Received from client: " + msg);
+
+            // Send response back to client
+            out.println("Server received: " + msg);
 
             socket.close(); // Close socket when done
         } catch (IOException e) {
